6832394fc054934583d5e2682055c3eb8e5d1c1e
likjjdblue <likjjdblue@gmail.com>
2020-06-14 11:53:30 +0800

    limit allowed class instance number

--- singleton_class_instance.py
+++ singleton_class_instance.py
@@ -18,7 +18,67 @@ def cache_cls_instance(cls):
     return cls
 
 
-@cache_cls_instance
+'''@cache_cls_instance
+class Foo:
+    def __init__(self, name):
+        self.name = name
+
+    def display(self):
+        print (self.name)
+
+    def bar(self,bar):
+        self.name = bar'''
+
+
+
+def limit_instance_number(cls_name = None, *, num = 1):
+    '''
+    Example 1:
+    @limit_instance_number
+    def FOO:
+       pass
+
+    Example 2:
+    @limit_instance_number
+    def FOO:
+       pass
+
+    Example 3:
+    @limit_instance_number(num = 3)
+    def FOO:
+       pass
+
+    :param cls_name:
+    :param num:
+    :return:
+    '''
+    def wrapper(cls):
+        class singleton:
+            current_instance_number = 0
+            def __init__(self, obj):
+                self.obj = obj
+
+            def __call__(self, *args, **kwargs):
+                tmpObj = None
+                if singleton.current_instance_number < num:
+                    tmpObj=self.obj.__new__(cls)
+                    singleton.current_instance_number += 1
+
+                return tmpObj
+        cls.__new__ = singleton(cls.__new__)
+        return cls
+
+    if not cls_name:
+        return wrapper
+    else:
+        return wrapper(cls_name)
+
+
+
+
+
+
+@limit_instance_number(num = 2)
 class Foo:
     def __init__(self, name):
         self.name = name
@@ -30,9 +90,11 @@ class Foo:
         self.name = bar
 
 
-a = Foo(name = 'a')
-b = Foo(name = 'b')
-b.bar('c')
-b.display()
-print (a is b)
+a = Foo('a')
+b = Foo('b')
+b.bar('B')
+a.display()
 
+
+print (a)
+print (b)
